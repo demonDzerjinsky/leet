adc0f3a7a04b07b1f1b73720dd96feffc9cbd999
dzerjinsky@mail.ru <dcherkunov@vtb.ru>
2025-10-15 11:02:40 +0700

    add: node weights

--- sum-dist-in-tree/src/test/java/ru/dzerjinsky/leetcode/SumDistTest.java
+++ sum-dist-in-tree/src/test/java/ru/dzerjinsky/leetcode/SumDistTest.java
@@ -13,13 +13,14 @@ public class SumDistTest {
         int[][] e = new int[][] { { 0, 1 }, { 0, 2 }, { 2, 3 }, { 2, 4 }, { 2, 5 } };
         int[] result = new int[n];
         int[] r = new int[n]; // i-element points to r[i] parent
+        int[] w = new int[n]; // weight
         r[0] = -1;
         for (int i = 1; i < n - 1; i++) {
             r[e[i][1]] = e[i][0];
         }
         assertThat(r).containsOnly(new int[] { -1, 0, 0, 2, 2, 2 });
         for (int cn = 0; cn < n; cn++) {
-            if (r[cn] != -1) {
+            if (r[cn] != -1) { // make tree with root in r[cn]
                 int ni = cn;
                 int buf = -1;
                 int nii;
@@ -30,9 +31,27 @@ public class SumDistTest {
                 }
                 r[ni] = buf;
             }
-            // make weightTree
+            // calc node weights
+            for (int i = 0; i < n; i++) {
+                if (r[i] == -1) {
+                    w[i] = 0;
+                }
+                int cw = 0;
+                int ci = i;
+                while (r[ci] != -1) {
+                    cw++;
+                    ci = r[ci];
+                }
+                w[i] = cw;
+            }
             // calc sum
+            System.out.println("tree");
             for (int i : r) {
+                System.out.print(String.format("%d ", i));
+            }
+            System.out.println();
+            System.out.println("weight");
+            for (int i : w) {
                 System.out.print(String.format("%d ", i));
             }
             System.out.println();
